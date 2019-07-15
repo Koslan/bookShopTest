62da5d7fe256414c98e562952743b21ba045b4b1
Константин <@DESKTOP-V8E4228>
2019-07-15 23:00:56 +0300

    1) Change language in view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,25 +4,25 @@ INSERT INTO Users (id, login, password) VALUES (1, 'admin', 'admin'),(2, 'user',
 
 INSERT INTO users_has_roles(users_id, roles_id) VALUES (1, 1),(1, 1);
 
-INSERT INTO Genres (id, name) VALUES (1, 'Романтика');
-INSERT INTO Genres (id, name) VALUES (2, 'Фантастика');
-INSERT INTO Genres (id, name) VALUES (3, 'Ужасы');
-INSERT INTO Genres (id, name) VALUES (4, 'Сказки');
-INSERT INTO Genres (id, name) VALUES (5, 'Публицистика');
-INSERT INTO Genres (id, name) VALUES (6, 'Учебники');
-INSERT INTO Genres (id, name) VALUES (7, 'Научная');
-INSERT INTO Genres (id, name) VALUES (8, 'Научно-популярная');
+INSERT INTO Genres (id, name) VALUES (1, 'Romance');
+INSERT INTO Genres (id, name) VALUES (2, 'Fantastic');
+INSERT INTO Genres (id, name) VALUES (3, 'Horror');
+INSERT INTO Genres (id, name) VALUES (4, 'Tales');
+INSERT INTO Genres (id, name) VALUES (5, 'Publicism');
+INSERT INTO Genres (id, name) VALUES (6, 'Tutorial');
+INSERT INTO Genres (id, name) VALUES (7, 'Scientific');
+INSERT INTO Genres (id, name) VALUES (8, 'Popular Science');
 
 
 INSERT INTO Books 
 (id, name, price, description)
 VALUES 
-(1, '1984', '410 грн', 'Written 70 years ago, 1984 was George Orwell chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than eve'),
-(2, 'A Brief History of Time', '300 грн', 'A landmark volume in science writing by one of the great minds of our time, Stephen Hawking book explores such profound questions as: How did the universe begin—and what made its start possible?'),
-(3, 'Alice Adventures in Wonderland', '239 uhy', 'In 1862 Charles Lutwidge Dodgson, a shy Oxford mathematician with a stammer, created a story about a little girl tumbling down a rabbit hole.'),
-(4, 'All the President Men', '400 грн', 'The full account of the Watergate scandal from the two Washington Post reporters who broke the story. This is “the work that brought down a presidency…perhaps the most influential piece of journalism in history.'),
-(5, 'Breath, Eyes, Memory', '590 грн', 'At an astonishingly young age, Edwidge Danticat has become one of our most celebrated new novelists, a writer who evokes the wonder, terror, and heartache '),
-(6, 'Catch-22: 50th Anniversary Edition', '110 грн', 'This fiftieth-anniversary edition commemorates Joseph Heller masterpiece with a new introduction; critical essays and reviews by Norman Mailer, Alfred Kazin,  and much more.');
+(1, '1984', '410 ₴', 'Written 70 years ago, 1984 was George Orwell chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than eve'),
+(2, 'A Brief History of Time', '300 ₴', 'A landmark volume in science writing by one of the great minds of our time, Stephen Hawking book explores such profound questions as: How did the universe begin—and what made its start possible?'),
+(3, 'Alice Adventures in Wonderland', '239 ₴', 'In 1862 Charles Lutwidge Dodgson, a shy Oxford mathematician with a stammer, created a story about a little girl tumbling down a rabbit hole.'),
+(4, 'All the President Men', '400 ₴', 'The full account of the Watergate scandal from the two Washington Post reporters who broke the story. This is “the work that brought down a presidency…perhaps the most influential piece of journalism in history.'),
+(5, 'Breath, Eyes, Memory', '590 ₴', 'At an astonishingly young age, Edwidge Danticat has become one of our most celebrated new novelists, a writer who evokes the wonder, terror, and heartache '),
+(6, 'Catch-22: 50th Anniversary Edition', '110 ₴', 'This fiftieth-anniversary edition commemorates Joseph Heller masterpiece with a new introduction; critical essays and reviews by Norman Mailer, Alfred Kazin,  and much more.');
 
 INSERT INTO Authors 
 (id, name) 
